3232f56fd67a397abb758bdee7c687f24be4eb53
Никита Межецкий <nsm020598@gmail.com>
2024-04-20 17:32:50 +1000

    Добавление маршрутизация на страницы Add Review

--- src/components/add-review-page/add-review-page.tsx
+++ src/components/add-review-page/add-review-page.tsx
@@ -1,4 +1,8 @@
+import { Addresses } from '../../const';
+import { Link, useParams } from 'react-router-dom';
+
 export default function AddReviewPage(): JSX.Element {
+  const params = useParams();
   return (
     <section className="film-card film-card--full">
       <div className="film-card__header">
@@ -10,17 +14,17 @@ export default function AddReviewPage(): JSX.Element {
 
         <header className="page-header">
           <div className="logo">
-            <a href="main.html" className="logo__link">
+            <Link to={Addresses.Main} className="logo__link">
               <span className="logo__letter logo__letter--1">W</span>
               <span className="logo__letter logo__letter--2">T</span>
               <span className="logo__letter logo__letter--3">W</span>
-            </a>
+            </Link>
           </div>
 
           <nav className="breadcrumbs">
             <ul className="breadcrumbs__list">
               <li className="breadcrumbs__item">
-                <a href="film-page.html" className="breadcrumbs__link">The Grand Budapest Hotel</a>
+                <Link to={`/films/${params.id}`} className="breadcrumbs__link">The Grand Budapest Hotel</Link>
               </li>
               <li className="breadcrumbs__item">
                 <a className="breadcrumbs__link">Add review</a>
@@ -31,11 +35,11 @@ export default function AddReviewPage(): JSX.Element {
           <ul className="user-block">
             <li className="user-block__item">
               <div className="user-block__avatar">
-                <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
+                <Link to={Addresses.MyList}><img src="img/avatar.jpg" alt="User avatar" width="63" height="63" /></Link>
               </div>
             </li>
             <li className="user-block__item">
-              <a className="user-block__link">Sign out</a>
+              <Link to={Addresses.SignIn} className="user-block__link">Sign out</Link>
             </li>
           </ul>
         </header>
