e4c096dced58f52fd65271d21368ebaf9c48bd01
Никита Межецкий <nsm020598@gmail.com>
2024-05-02 00:54:25 +1000

    Создадим компоненты для movie page

---
+++ src/components/movie-page/components-movie-page/components-reviews-movie-page/component-reviews-movie-page.tsx
@@ -0,0 +1,21 @@
+import { TypeMoviePage } from '../../../../mocks/type-mocks';
+import { TypeReviews, } from '../../../../mocks/type-mocks';
+import ReviewComponent from './review-component';
+
+type TypePropsReviewsMoviePage = {
+  movie: TypeMoviePage;
+  reviews: TypeReviews;
+};
+
+export default function ComponentReviewsMoviePage({ movie, reviews }: TypePropsReviewsMoviePage): JSX.Element {
+  const comments = reviews.find((item) => movie.id === item.movieId);
+  console.log(reviews)
+  return (
+    <div className="film-card__reviews film-card__row">
+      {comments ?
+        <div className="film-card__reviews-col">
+          {comments.comments.map((item) => (<ReviewComponent key={item.id} comment={item} />))}
+        </div> : ''}
+    </div>
+  );
+}
---
+++ src/components/movie-page/components-movie-page/components-reviews-movie-page/review-component.tsx
@@ -0,0 +1,22 @@
+import { TypeComment } from '../../../../mocks/type-mocks';
+import { formatDate } from '../../../../const';
+import { getDateFormate } from '../../../../mocks/utils-mocks';
+type TypePropsReviewComponent = {
+  comment: TypeComment;
+}
+export default function ReviewComponent({comment}: TypePropsReviewComponent): JSX.Element {
+  return (
+    <div className="review">
+      <blockquote className="review__quote">
+        <p className="review__text">{comment.comment}</p>
+
+        <footer className="review__details">
+          <cite className="review__author">{comment.user}</cite>
+          <time className="review__date" dateTime={getDateFormate(comment.date, formatDate.dataReviewDataTime)}>{getDateFormate(comment.date, formatDate.dataReview)}</time>
+        </footer>
+      </blockquote>
+
+      <div className="review__rating">{comment.rating}</div>
+    </div>
+  );
+}
