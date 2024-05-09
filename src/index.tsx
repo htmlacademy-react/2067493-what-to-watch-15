987a6e3758da0f821d3e1c50e315ba139bb57806
Никита Межецкий <nsm020598@gmail.com>
2024-05-09 20:50:32 +1000

    Подключим хранилище к проекту

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 import { moviesCards, moviesPage, moviesFavorite, promoMovie } from './mocks/films';
 import { reviews } from './mocks/reviews';
+import { store } from './store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,6 +12,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App promoMovie={promoMovie} movies={moviesCards} moviesFavorite={moviesFavorite} moviesPage={moviesPage} reviews={reviews}/>
+    <Provider store={store}>
+      <App promoMovie={promoMovie} movies={moviesCards} moviesFavorite={moviesFavorite} moviesPage={moviesPage} reviews={reviews}/>
+    </Provider>
   </React.StrictMode>
 );
